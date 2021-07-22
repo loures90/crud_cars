7a172b5c4162e5ededad523a499709147f85c7ea
Fernando <Fernando>
2021-07-22 09:04:27 -0300

    feat: route of filters is working

--- src/business/filterBusiness.js
+++ src/business/filterBusiness.js
@@ -1,7 +1,28 @@
 
-const filterCarBusiness = async (query, filterCarsDataBase) => {
+const filterCarBusiness = async (query, filterCarsDataBase, filterCarRangeDataBase) => {
     try {
-        return await filterCarsDataBase(query)
+        if (!query.greaterThan && !query.lessThan)
+            return await filterCarsDataBase(query)
+        let range = { ...query }
+        if (query.year && query.greaterThan) {
+            range.year = { $gt: Number(query.year) }
+            delete range.greaterThan
+        }
+        if (query.year && query.lessThan){
+            range.year = { $lt: Number(query.year) }
+            delete range.lessThan
+        }
+        if (query.salePrice && query.greaterThan){
+            range.salePrice = { $gt: Number(query.salePrice) }
+            delete range.greaterThan
+        }
+        if (query.salePrice && query.lessThan){
+            range.salePrice = { $lt: Number(query.salePrice) }
+            delete range.lessThan
+        }
+        console.log("range", range)
+        return await filterCarRangeDataBase(range)
+
     } catch (error) {
         throw error
     }
--- src/controller/filterController.js
+++ src/controller/filterController.js
@@ -1,10 +1,11 @@
 import filterCarBusiness from "../business/filterBusiness.js"
 import filterCarDataBase from "../database/filterData.js"
+import filterCarRangeDataBase from "../database/filterRangeData.js"
 
 const filterCarController = async (req, res) => {
     try {
         const query = req.query
-        const result = await filterCarBusiness(query, filterCarDataBase)
+        const result = await filterCarBusiness(query, filterCarDataBase, filterCarRangeDataBase)
         res.status(200).send(result)
     } catch (error) {
         res.status(400).send({ error: error.message })
